c47f0ce112939c24d3e4dc73aa16beeffdd4ac56
Chibuzor Obiora <cobiora@getcarbon.co>
2019-11-10 11:22:24 +0100

    Tutorial completion on svelte

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Board from "./Board.svelte";
+	const title = "Tic Tac Toe";
+</script>
+
+<style>
+</style>
+
+<svelte:head>
+<title>{title}</title>
+</svelte:head>
+<h1>{title}</h1>
+<Board />
